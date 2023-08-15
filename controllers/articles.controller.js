b879aa3f3737479a01582adc11e9f8faebe4f99f
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-08-15 11:05:03 +0100

    completes task 5

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,10 +1,20 @@
-const { selectArticleByID } = require("../models");
+const { selectArticleByID, selectArticles } = require("../models");
 
 exports.getArticleByID = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleByID(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticles = (req, res, next) => {
+  selectArticles()
+    .then(({ rows: articles }) => {
+      res.status(200).send({ articles });
     })
     .catch((err) => {
       next(err);
